0eeca99640c5060869e5e20ba4423abdbd4c4bc0
orenmagid <autoimpedicis@gmail.com>
2018-08-17 15:55:52 -0400

    Starting to generate teams.

--- src/Components/UserComparisonCard.js
+++ src/Components/UserComparisonCard.js
@@ -1,40 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function UserComparisonCard({ user, comparisons }) {
-  let userComparisons = comparisons.filter(
-    comparison => comparison.user_id === user.id
-  );
+export default class UserComparisonCard extends Component {
+  state = {
+    active: false
+  };
 
-  let sum = 0;
-  for (let i = 0; i < userComparisons.length; i++) {
-    sum += parseInt(userComparisons[i].pie, 10);
-  }
+  handleToggle = () => {
+    let active = this.state.active;
+    if (active) {
+      this.props.removeFromActivePlayers(this.props.user);
+    } else {
+      this.props.addToActivePlayers(this.props.user);
+    }
+    this.setState({
+      active: !active
+    });
+  };
 
-  let avg = sum / userComparisons.length;
+  render() {
+    let { user, comparisons } = this.props;
 
-  console.log("avg", avg);
+    let userComparisons = comparisons.filter(
+      comparison => comparison.user_id === user.id
+    );
 
-  return (
-    <React.Fragment>
-      <div className="ui card blue ">
-        <div className="content">
-          {/* <img
-            className="ui medium rounded image"
-            src={url}
-            alt="player-picture"
-          /> */}
-          <div className="header">{user.name}</div>
-          <div className="meta" />
-          <div className="description">
-            <p>Height: {user.height} Inches</p>
-            {userComparisons.length !== 0 ? (
-              <p>Average PIE of Comparisons: {avg.toFixed(2)}</p>
-            ) : null}
+    let sum = 0;
+    for (let i = 0; i < userComparisons.length; i++) {
+      sum += parseInt(userComparisons[i].pie, 10);
+    }
+
+    let avg = sum / userComparisons.length;
+
+    console.log("avg", avg);
+
+    return (
+      <React.Fragment>
+        <div className="ui card blue ">
+          <div className="content">
+            {/* <img
+              className="ui medium rounded image"
+              src={url}
+              alt="player-picture"
+            /> */}
+            <div className="header">{user.name}</div>
+            <div className="meta" />
+            <div className="description">
+              <p>Height: {user.height} Inches</p>
+              {userComparisons.length !== 0 ? (
+                <React.Fragment>
+                  <p>Average PIE of Comparisons: {avg.toFixed(2)}</p>
+                  <p>Number of Comparisons: {userComparisons.length}</p>
+                </React.Fragment>
+              ) : null}
+            </div>
+            <div className="extra content" />
+            <br />
+            <div className="ui toggle checkbox">
+              <input
+                type="checkbox"
+                name="active"
+                onChange={this.handleToggle}
+              />
+              <label>Currently Playing?</label>
+            </div>
           </div>
-          <div className="extra content" />
-          <br />
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
--- src/Containers/ShowComparisonsContainer.js
+++ src/Containers/ShowComparisonsContainer.js
@@ -2,22 +2,61 @@ import React, { Component } from "react";
 import UserComparisonCard from "../Components/UserComparisonCard";
 
 export default class ShowComparisonsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    activePlayers: []
+  };
+
+  addToActivePlayers = user => {
+    this.setState({
+      activePlayers: [...this.state.activePlayers, user]
+    });
+  };
+
+  removeFromActivePlayers = user => {
+    let activePlayers = this.state.activePlayers;
+    let index = activePlayers.indexOf(user);
+    activePlayers.splice(index, 1);
+    this.setState({
+      activePlayers: activePlayers
+    });
+  };
+
+  getEvenNumOfPlayers = () => {
+    let activePlayers = [...this.state.activePlayers];
+    if (activePlayers.length >= 4) {
+      if (activePlayers.length % 2 !== 0) {
+        activePlayers.splice(
+          (Math.floor(Math.random() * this.state.activePlayers.length), 1)
+        );
+      }
+      this.generateTeams();
+    }
+  };
 
   render() {
     return (
-      <div className="ui three doubling stackable cards">
-        {this.props.users.map(user => {
-          return (
-            <UserComparisonCard
-              user={user}
-              comparisons={this.props.comparisons}
-            />
-          );
-        })}
+      <div>
+        <br />
+        <button
+          // onClick={this.triggerReRender}
+          className="ui secondary basic button"
+        >
+          Generate Teams
+        </button>
+        <br />
+        <div className="ui three doubling stackable cards">
+          {this.props.users.map(user => {
+            return (
+              <UserComparisonCard
+                addToActivePlayers={this.addToActivePlayers}
+                removeFromActivePlayers={this.removeFromActivePlayers}
+                key={user.id}
+                user={user}
+                comparisons={this.props.comparisons}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
