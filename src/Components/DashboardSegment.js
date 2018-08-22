d8f633c82aefe4d42d9a92b2a6ca90f97b4e1250
orenmagid <autoimpedicis@gmail.com>
2018-08-21 21:33:26 -0400

    Edited Dashboard Segment text.

--- src/Components/DashboardSegment.js
+++ src/Components/DashboardSegment.js
@@ -6,7 +6,7 @@ const DashboardSegment = () => (
     Team Splitter is for groups that play basketball together. To join a group,
     ask someone in the group for the group name and number. You can also create
     your own group and add your friends. Select "Make Comparisons" to compare
-    the players in your group to NBA players. Select "Show Comparisons"
+    the players in your group to NBA players. Select "Show Comparisons" to see
     everyone's average comparisons and generate teams based on these
     comparisons.
   </Segment>
